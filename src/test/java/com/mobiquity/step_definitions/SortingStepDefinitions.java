b0c50a7bbf0a64e13ab08517598110f85f49d518
furkisaviour <furkankayakho@gmail.com>
2022-08-03 21:56:32 +0200

    Hooks and BrowserUtils was added

---
+++ src/test/java/com/mobiquity/step_definitions/SortingStepDefinitions.java
@@ -0,0 +1,30 @@
+package com.mobiquity.step_definitions;
+
+import com.mobiquity.pages.WomenPage;
+import io.cucumber.java.en.*;
+
+public class SortingStepDefinitions {
+
+    WomenPage womenPage = new WomenPage();
+
+    @Given("user is on main page")
+    public void user_is_on_main_page() {
+
+    }
+    @When("user clicks women page")
+    public void user_clicks_women_page() {
+
+    }
+    @When("user clicks Sort by button")
+    public void user_clicks_sort_by_button() {
+
+    }
+    @When("user clicks highest first")
+    public void user_clicks_highest_first() {
+
+    }
+    @Then("user should see highest product")
+    public void user_should_see_highest_product() {
+
+    }
+}
---
+++ src/test/java/com/mobiquity/utilities/BrowserUtils.java
@@ -0,0 +1,37 @@
+package com.mobiquity.utilities;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BrowserUtils {
+
+    public static void wait(int second) {
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+            System.out.println("something happened in sleep method");
+
+        }
+    }
+
+    public static void waitForPageToLoad(long timeOutInSeconds) {
+        ExpectedCondition<Boolean> expectation = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+            }
+        };
+        try {
+            System.out.println("Waiting for page to load...");
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeOutInSeconds);
+            wait.until(expectation);
+        } catch (Throwable error) {
+            System.out.println(
+                    "Timeout waiting for Page Load Request to complete after " + timeOutInSeconds + " seconds");
+        }
+    }
+}
